e2216307145c2e4f499ff7dfcc6744516980215e
lilili001 <2861166132@qq.com>
2018-10-12 16:32:15 +0800

    edit controller

--- app/Http/Controllers/WeixinController.php
+++ app/Http/Controllers/WeixinController.php
@@ -5,20 +5,19 @@ namespace App\Http\Controllers;
 use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 
-class WexinController extends Controller
+class WeixinController extends Controller
 {
     //微信登陆api
     public function login()
     {
         $appid = config('wx.xiaochengxu.appid');
-        $appsecret = request('wx.xiaochengxu.appsecret');
+        $appsecret = config('wx.xiaochengxu.appsecret');
         $jscode = request('js_code');
+        $url = "https://api.weixin.qq.com/sns/jscode2session?appid={$appid}&secret={$appsecret}&js_code=$jscode&grant_type=authorization_code";
 
-        $url = 'https://api.weixin.qq.com/sns/jscode2session?appid=$appid&secret=$appsecret&js_code=$jscode&grant_type=authorization_code';
-        dd($url);
         $client = new Client();
         $response = $client->request('get',$url);
         $response = $response->getBody()->getContents();
-        dd($response);
+        return response()->json($response);
     }
 }
